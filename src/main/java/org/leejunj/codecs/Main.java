20ab4cb2f1d23f05355576db0f809d80e4bc5000
zunger <lijunjie1680@gmail.com>
2024-05-03 01:46:40 +0800

    add .gitignore
    Signed-off-by: zunger <lijunjie1680@gmail.com>

---
+++ src/main/java/org/leejunj/codecs/Main.java
@@ -0,0 +1,12 @@
+package org.leejunj.codecs;
+
+/**
+ * Description：
+ * author：李俊杰
+ * Created on 2024/5/3 1:35
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
